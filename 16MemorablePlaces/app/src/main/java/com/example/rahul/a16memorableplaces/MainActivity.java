62c4591cf423ebc7589efc40134b798b45576556
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2018-11-06 13:31:28 +0530

    not working state

---
+++ 16MemorablePlaces/app/src/main/java/com/example/rahul/a16memorableplaces/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.rahul.a16memorableplaces;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> places;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.listview1);
+        places = new ArrayList<String>();
+        places.add("Add a new place");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,places);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                intent.putExtra("placeNumber",i);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+}
---
+++ 16MemorablePlaces/app/src/main/java/com/example/rahul/a16memorableplaces/MapsActivity.java
@@ -0,0 +1,137 @@
+package com.example.rahul.a16memorableplaces;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+                {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centerMapOnLocation(lastKnownLocation, "YourLocation");
+
+            }
+        }
+    }
+
+    public void centerMapOnLocation(Location location, String title) {
+        if(location != null){
+
+            mMap.clear();
+            LatLng userLocation = new LatLng(10,10);
+//            LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+//            mMap.addMarker(new MarkerOptions().position(userloc).title("MyCurrentLocation").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+//            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userloc,1));
+//            LatLng userLocation = new LatLng(10,10);
+//
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 1));
+            Toast.makeText(getApplicationContext(),"if",Toast.LENGTH_SHORT).show();
+         //
+        }
+        else
+        {
+
+            Toast.makeText(getApplicationContext(),"else",Toast.LENGTH_SHORT).show();
+
+            mMap.clear();
+            LatLng userLocation = new LatLng(10,10);
+//            LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+//            mMap.addMarker(new MarkerOptions().position(userloc).title("MyCurrentLocation").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+//            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userloc,1));
+//            LatLng userLocation = new LatLng(10,10);
+//
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 1));
+            //
+        }
+           // Toast.makeText(getApplicationContext(),"Location is null",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Intent intent = getIntent();
+        if (intent.getIntExtra("placeNumber", 0) == 0) {
+            //zoon in the user location
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    centerMapOnLocation(location, "Your Location...");
+
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            };
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centerMapOnLocation(lastKnownLocation, "YourLocation");
+            }
+
+
+
+
+
+        }
+    }
+}
